10e555508f69d729b720fdee46b39bf5fbb90aac
John Fristedt <john.fristedt@chas.se>
2016-10-05 10:43:31 +0200

    Started workshop

---
+++ ServicesDirectivesFiltersWorkshop/Scripts/Controllers/home.controller.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("mainModule")
+    .controller("HomeController", [
+        "$scope",
+        "postsApi",
+        function ($scope, postsApi) {
+            $scope.title = "Home";
+            $scope.posts = [];
+
+            postsApi.getPosts()
+                .then(function (data) {
+                    $scope.posts = data;
+                });
+
+        }
+    ]);
